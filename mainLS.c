c259a0bd17e2dc0809b41e3f009d7cb06a54a9f3
RobbieLopez <55214007+RobbieLopez@users.noreply.github.com>
2019-10-17 22:17:05 -0500

    Lista Simple Ligada
    Ejemplo de lista simple ligada con menu

---
+++ mainLS.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+typedef struct Lista
+{
+  int dato;
+  struct Lista *siguiente;
+} Lista;
+
+Lista *primero = NULL;
+Lista *ultimo = NULL;
+
+void
+insertarLista ()
+{
+  Lista *nuevo = (Lista *) malloc (sizeof (Lista));
+  printf (" Ingrese el dato que tendra el nuevo nodo de la lista: ");
+  scanf ("%d", &nuevo->dato);
+  if (primero == NULL)
+    {
+      primero = nuevo;
+      primero->siguiente = NULL;
+      ultimo = nuevo;
+    }
+  else
+    {
+      ultimo->siguiente = nuevo;
+      nuevo->siguiente = NULL;
+      ultimo = nuevo;
+    }
+  printf ("Dato ingresado\n");
+}
+
+void
+mostrarLista ()
+{
+  Lista *actual = (Lista *) malloc (sizeof (Lista));
+  actual = primero;
+  if (primero != NULL)
+    {
+      while (actual != NULL)
+	{
+	  printf ("\n %d\n ", actual->dato);
+	  actual = actual->siguiente;
+	}
+    }
+  else
+    {
+      printf ("\n La lista esta vacia\n");
+    }
+}
+
+void
+eliminarDato ()
+{
+  Lista *actual = (Lista *) malloc (sizeof (Lista));
+  actual = primero;
+  Lista *anterior = (Lista *) malloc (sizeof (Lista));
+  anterior = NULL;
+  int datoB = 0, datoE = 0;
+  printf (" Ingrese el dato de la lista para eliminar: ");
+  scanf ("%d", &datoB);
+  if (primero != NULL)
+    {
+      while (actual != NULL && datoE != 1)
+	{
+
+	  if (actual->dato == datoB)
+	    {
+	      if (actual == primero)
+		{
+		  primero = primero->siguiente;
+		}
+	      else if (actual == ultimo)
+		{
+		  anterior->siguiente = NULL;
+		  ultimo = anterior;
+		}
+	      else
+		{
+		  anterior->siguiente = actual->siguiente;
+		}
+	      printf ("\n");
+	      datoE = 1;
+	    }
+	  anterior = actual;
+	  actual = actual->siguiente;
+	}
+      if (datoE == 0)
+	{
+	  printf ("\n");
+	}
+      else
+	{
+	  free (anterior);
+	}
+
+    }
+  else
+    {
+      printf ("\n");
+    }
+}
+
+int op;
+int
+main (int argc, char *argv[])
+{
+  do
+    {
+      printf ("1.- Agregar datos\n");
+      printf ("2.- Mostrar Lista\n");
+      printf ("3.- Eliminar Dato\n");
+      printf ("E.- Salir\n");
+      scanf ("%d", &op);
+
+      switch (op)
+	{
+	case 1:
+	  {
+	    insertarLista ();
+	  }
+	  break;
+	case 2:
+	  {
+	    mostrarLista ();
+	  }
+	  break;
+	case 3:
+	  {
+	    eliminarDato ();
+	  }
+	  break;
+	}
+    }
+  while (op != 'E');
+
+
+  return 0;
+}
